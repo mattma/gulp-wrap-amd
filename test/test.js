d3d29378e5313d0672b6b333f47eae82046db4e6
Matt Ma <bigmabig@gmail.com>
2015-01-22 08:29:18 -0800

    update the unit test of test.js with lab framework and code assertion library

--- test/test.js
+++ test/test.js
@@ -25,13 +25,10 @@ function expectStream(options, done){
     contents: null,
     name: null
   });
-  console.log('options: ', options);
   return through.obj(function(file, enc, cb){
     options.contents = fs.readFileSync(file.path, 'utf-8');
     var expected = _.template(jst, options);
     var results = String(file.contents);
-    console.log('expected: ', expected);
-    console.log('results: ', results);
     expect(results).to.deep.equal(expected);
     cb();
     done();
@@ -39,62 +36,61 @@ function expectStream(options, done){
 }
 
 describe('WrapAmd module', function(){
-  // it('should wrap a function in simple AMD wrapper', function(done){
-  //   gulp.src(filename)
-  //     .pipe(task())
-  //     .pipe(expectStream({}, done));
-  // });
+  it('should wrap a function in simple AMD wrapper', function(done){
+    gulp.src(filename)
+      .pipe(task())
+      .pipe(expectStream({}, done));
+  });
 
-  // it('should wrap a function in simple AMD wrapper if missing deps but has params', function(done){
-  //   var opts = {
-  //     params: ['jade']
-  //   };
-  //   gulp.src(filename)
-  //     .pipe(task(opts))
-  //     .pipe(expectStream({}, done));
-  // });
+  it('should wrap a function in simple AMD wrapper if missing deps but has params', function(done){
+    var opts = {
+      params: ['jade']
+    };
+    gulp.src(filename)
+      .pipe(task(opts))
+      .pipe(expectStream({}, done));
+  });
 
-  // it('should wrap a function in AMD wrapper with custom deps and params', function(done){
-  //   var opts = {
-  //     deps: ['jade'],
-  //     params: ['jade']
-  //   };
-  //   gulp.src(filename)
-  //     .pipe(task(opts))
-  //     .pipe(expectStream(opts, done));
-  // });
+  it('should wrap a function in AMD wrapper with custom deps and params', function(done){
+    var opts = {
+      deps: ['jade'],
+      params: ['jade']
+    };
+    gulp.src(filename)
+      .pipe(task(opts))
+      .pipe(expectStream(opts, done));
+  });
 
-  // it('should wrap a function in AMD wrapper with custom deps', function(done){
-  //   var opts = {
-  //     deps: ['domReady!']
-  //   };
-  //   gulp.src(filename)
-  //     .pipe(task(opts))
-  //     .pipe(expectStream(opts, done));
-  // });
+  it('should wrap a function in AMD wrapper with custom deps', function(done){
+    var opts = {
+      deps: ['domReady!']
+    };
+    gulp.src(filename)
+      .pipe(task(opts))
+      .pipe(expectStream(opts, done));
+  });
 
-  // it('should wrap a function in AMD wrapper with custom return variable', function(done){
-  //   var opts = {
-  //     exports: 'helloWorld'
-  //   };
-  //   gulp.src(exportFilename)
-  //     .pipe(task(opts))
-  //     .pipe(expectStream(opts, done));
-  // });
+  it('should wrap a function in AMD wrapper with custom return variable', function(done){
+    var opts = {
+      exports: 'helloWorld'
+    };
+    gulp.src(exportFilename)
+      .pipe(task(opts))
+      .pipe(expectStream(opts, done));
+  });
 
-  // it('should isolate the contents of the individual files', function(done){
-  //     var opts = {
-  //       deps: ['test']
-  //     };
-  //     gulp.src(path.join(__dirname, './fixtures/test-*.js'))
-  //       .pipe(task(opts))
-  //       .pipe(expectStream(opts, done));
-  // });
+  it('should isolate the contents of the individual files', function(done){
+      var opts = {
+        deps: ['test']
+      };
+      gulp.src(path.join(__dirname, './fixtures/test-*.js'))
+        .pipe(task(opts))
+        .pipe(expectStream(opts, done));
+  });
 
-  // @TODO Problem issue here
   it('should include module name if moduleRoot option is given', function(done) {
     var opts = {
-      moduleRoot: './',
+      moduleRoot: 'test/',
       deps: ['jade'],
       params: ['jade']
     };
@@ -108,85 +104,85 @@ describe('WrapAmd module', function(){
       .pipe(expectStream(exportOpts, done));
   });
 
-  // it('module name should be relative to moduleRoot', function(done) {
-  //   var opts = {
-  //     moduleRoot: 'fixtures/',
-  //     deps: ['jade'],
-  //     params: ['jade']
-  //   };
-  //   var exportOpts = {
-  //     deps: ['jade'],
-  //     params: ['jade'],
-  //     name: 'helloworld'
-  //   };
-  //   gulp.src(filename)
-  //     .pipe(task(opts))
-  //     .pipe(expectStream(exportOpts, done));
-  // });
+  it('module name should be relative to moduleRoot', function(done) {
+    var opts = {
+      moduleRoot: 'test/fixtures/',
+      deps: ['jade'],
+      params: ['jade']
+    };
+    var exportOpts = {
+      deps: ['jade'],
+      params: ['jade'],
+      name: 'helloworld'
+    };
+    gulp.src(filename)
+      .pipe(task(opts))
+      .pipe(expectStream(exportOpts, done));
+  });
+
+  it('modulePrefix option requires moduleRoot existence', function(done) {
+    var opts = {
+      modulePrefix: 'rocks/',
+      deps: ['jade'],
+      params: ['jade']
+    };
+    var exportOpts = {
+      deps: ['jade'],
+      params: ['jade'],
+      name: null
+    };
+    gulp.src(filename)
+      .pipe(task(opts))
+      .pipe(expectStream(exportOpts, done));
+  });
+
+  it('should prepend the modulePrefix in the module name defination', function(done) {
+    var opts = {
+      moduleRoot: 'test/fixtures/',
+      modulePrefix: 'rocks/',
+      deps: ['jade'],
+      params: ['jade']
+    };
+    var exportOpts = {
+      deps: ['jade'],
+      params: ['jade'],
+      name: 'rocks/helloworld'
+    };
+    gulp.src(filename)
+      .pipe(task(opts))
+      .pipe(expectStream(exportOpts, done));
+  });
+
+  it('should add trailing slash to modulePrefix if not existed in the module name defination', function(done) {
+    var opts = {
+      moduleRoot: 'test/fixtures/',
+      modulePrefix: 'rocks',
+      deps: ['jade'],
+      params: ['jade']
+    };
+    var exportOpts = {
+      deps: ['jade'],
+      params: ['jade'],
+      name: 'rocks/helloworld'
+    };
+    gulp.src(filename)
+      .pipe(task(opts))
+      .pipe(expectStream(exportOpts, done));
+  });
+
+  it('does not have windows separaters in moduleId', function(done){
+    var opts = {
+      moduleRoot: 'test/fixtures/',
+      modulePrefix: 'rocks',
+      deps: ['jade'],
+      params: ['jade']
+    };
+    gulp.src(filename)
+      .pipe(task(opts))
+      .pipe(through.obj(function(file, enc, cb){
+        expect(!!~String(file.contents).indexOf("rocks/helloworld")).to.be.true();
+        cb();
+        done();
+      }));
+  });
 });
-
-// test('modulePrefix option requires moduleRoot existence', function(t) {
-//   t.plan(1);
-
-//   gulp.src(filename)
-//     .pipe(task({
-//       modulePrefix: 'rocks/',
-//       deps: ['jade'],
-//       params: ['jade']
-//     }))
-//     .pipe(expectStream(t, {
-//       deps: ['jade'],
-//       params: ['jade'],
-//       name: null
-//     }));
-// });
-
-// test('should prepend the modulePrefix in the module name defination', function(t) {
-//   t.plan(1);
-
-//   gulp.src(filename)
-//     .pipe(task({
-//       moduleRoot: 'fixtures/',
-//       modulePrefix: 'rocks/',
-//       deps: ['jade'],
-//       params: ['jade']
-//     }))
-//     .pipe(expectStream(t, {
-//       deps: ['jade'],
-//       params: ['jade'],
-//       name: 'rocks/helloworld'
-//     }));
-// });
-
-// test('should add trailing slash to modulePrefix if not existed in the module name defination', function(t) {
-//   t.plan(1);
-
-//   gulp.src(filename)
-//     .pipe(task({
-//       moduleRoot: 'fixtures/',
-//       modulePrefix: 'rocks',
-//       deps: ['jade'],
-//       params: ['jade']
-//     }))
-//     .pipe(expectStream(t, {
-//       deps: ['jade'],
-//       params: ['jade'],
-//       name: 'rocks/helloworld'
-//     }));
-// });
-
-// test('does not have windows separaters in moduleId', function(t){
-//   t.plan(1);
-
-//   gulp.src(filename)
-//     .pipe(task({
-//       moduleRoot: 'fixtures/',
-//       modulePrefix: 'rocks',
-//       deps: ['jade'],
-//       params: ['jade']
-//     }))
-//     .pipe(through.obj(function(file, enc, cb){
-//       t.equals(!!~String(file.contents).indexOf("rocks/helloworld"), true);
-//       cb();
-//     }));
-// });
